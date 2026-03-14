cd18097a1272d0d2b36e1572541d0ce700e647a0
Теодор Димитров <tedobearmini@gmail.com>
2026-03-14 20:33:08 +0200

    fixed AoE again :(

--- AoE/AoEScale.cpp
+++ AoE/AoEScale.cpp
@@ -1,72 +1,38 @@
-#include "AoEScale.h"
-#include <iostream>
-#include <limits>
+#include <vector>
 #include <queue>
 #include <cmath>
-#include <set>
-const size_t PATH_LENGTH = 2;
+#include <algorithm>
+#include "../algorithms/graph_structure.hpp"
+#include "AoEScale.h"
+using namespace std;
 
-
-// void printGraph(const vector<vector<pair<size_t,size_t>>>& adj) {
-//     size_t V = adj.size();
-//     for (size_t i=0;i<V;i++) {
-//         size_t numNeigh = adj[i].size();
-//         for (size_t j=0;j<numNeigh;j++) {
-//             std::cout<<"("<<adj[i][j].first<<","<<adj[i][j].second<<") ";
-//         }
-//         std::cout<<std::endl;
-//     }
-// }
-
-
-std::queue<vector<Edge>> bfs(vector<vector<Edge>>& adj, const Node& src, vector<double>& dist) {
+void updateNearEdgesV2(vector<vector<Edge>>& adj, const size_t& srcNodeId, const double& modifier) {
     size_t V = adj.size();
-    vector<int> visited(V, 0);
+    size_t PATH_LENGTH = 2;
 
-
-    for (int i=0;i<dist.size();i++) dist[i] = numeric_limits<double>::infinity();
-    dist[src.id]=0;
-    std::queue<vector<Edge>> q;
-    visited[src.id] = 2;
-    vector<Edge> currPath;
-    for (int i=0;i<adj[src.id].size();i++) {
-        currPath.push_back(adj[src.id][i]);
-        q.push(currPath);
-        currPath.pop_back();
-    }
+    vector<int> hops(V, -1);
+    queue<size_t> q;
+    hops[srcNodeId] = 0;
+    q.push(srcNodeId);
 
     while (!q.empty()) {
-        if (q.front().size()>=PATH_LENGTH) break;
-        currPath = q.front();
+        size_t u = q.front();
         q.pop();
+        int current_hop = hops[u];
 
-        for (auto x : adj[currPath.back().second_node.id]) {
-            if (!visited[x.second_node.id]) {
-                dist[x.second_node.id] = dist[currPath.back().second_node.id] + x.length;
-                visited[x.second_node.id] = 1;
-                currPath.push_back(x);
-                q.push(currPath);
-                currPath.pop_back();
-            }
-        }
-        visited[currPath.back().second_node.id]=2;
-    }
-    return q;
-}
+        if (current_hop > PATH_LENGTH) continue;
 
-void updateNearEdgesV2(vector<vector<Edge>> &adj, const Node &srcNode, const size_t &modifier) {
-    vector<double> dist(adj.size());
-    std::queue<vector<Edge>> bfsResult = bfs(adj,srcNode,dist);
-    std::set<pair<size_t,size_t>> updatedEdges;
+        double decay_factor = pow(0.4, current_hop + 1);
+        double added_danger = modifier * decay_factor;
 
-    while (!bfsResult.empty()) {
-        vector<Edge> currPath = bfsResult.front();
-        bfsResult.pop();
-        for (int i=0;i<PATH_LENGTH;i++) {
-            if (updatedEdges.find({currPath[i].first_node.id,currPath[i].second_node.id})==updatedEdges.end()) {
-                currPath[i].rating += modifier*pow(0.4,i+1);
-                currPath[i].rating = std::min(currPath[i].rating,10.0);
-                updatedEdges.insert({currPath[i].first_node.id,currPath[i].second_node.id});
+        for (Edge& edge : adj[u]) {
+            size_t v = edge.second_node.id;
+            edge.rating += added_danger;
+            edge.rating = std::min(edge.rating, 10.0);
+
+            if (hops[v] == -1) {
+                hops[v] = current_hop + 1;
+                q.push(v);
             }
         }
     }
--- AoE/AoEScale.h
+++ AoE/AoEScale.h
@@ -3,14 +3,14 @@
 
 #include <vector>
 #include <queue>
-#include "..\algorithms/graph_structure.hpp"
+#include "../algorithms/graph_structure.hpp"
 using namespace std;
 
 // void updateNearEdges(vector<vector<pair<size_t,size_t>>>& adj, const pair<size_t,size_t>& srcEdge, const size_t& diffInRating);
 // void updateOppositeSide(vector<vector<pair<size_t,size_t>>>& adj, const size_t& v1, const size_t& other, double modifier);
 // void printGraph(const vector<vector<pair<size_t,size_t>>>& adj);
 
-void updateNearEdgesV2(vector<vector<Edge>> &adj, const Node &srcNode, const size_t &modifier);
+void updateNearEdgesV2(vector<vector<Edge>>& adj, const size_t& srcNodeId, const double& modifier);
 std::queue<vector<Edge>> bfs(vector<vector<Edge>>& adj, const Node& src, vector<double>& dist);
 
 #endif //GIT_PUSH_AND_PRAY_AOESCALE_H
