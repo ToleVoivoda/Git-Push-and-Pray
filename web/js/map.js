94814e08921d55bdf3a3dd3948fae639dfab9dec
katerinacookies <sokolyt@gmail.com>
2026-03-15 00:38:57 +0200

    map stuff

---
+++ web/js/map.js
@@ -0,0 +1,26 @@
+//initialize map
+const map = L.map('map').setView([42.6977, 23.3219], 13); // Sofia coordinates
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '© OpenStreetMap'
+}).addTo(map);
+
+//map click - get lat/lng and place nodes
+let points = []; 
+
+map.on('click', function(e) {
+    points.push(e.latlng);
+    L.marker(e.latlng).addTo(map);
+
+    if (points.length === 2) {
+        //start/end point, cpp call for path @Aydin
+        fetchPath(points[0], points[1]);
+        points = []; // Reset for next selection
+    }
+});
+
+//path request
+
+
+
+//
