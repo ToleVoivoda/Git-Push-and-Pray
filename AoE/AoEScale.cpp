f00c821cc0e4bcc952de94958cbc9656b57cecb8
Теодор Димитров <tedobearmini@gmail.com>
2026-03-14 22:09:10 +0200

    Final Testing AoE

--- AoE/AoEScale.cpp
+++ AoE/AoEScale.cpp
@@ -4,6 +4,7 @@
 #include <algorithm>
 #include "../algorithms/graph_structure.hpp"
 #include "AoEScale.h"
+#include <iostream>
 using namespace std;
 
 void updateNearEdgesV2(vector<vector<Edge>>& adj, const size_t& srcNodeId, const double& modifier) {
@@ -22,18 +23,39 @@ void updateNearEdgesV2(vector<vector<Edge>>& adj, const size_t& srcNodeId, const
 
         if (current_hop > PATH_LENGTH) continue;
 
-        double decay_factor = pow(0.4, current_hop + 1);
+        double decay_factor = pow(0.25, current_hop + 1);
         double added_danger = modifier * decay_factor;
 
         for (Edge& edge : adj[u]) {
             size_t v = edge.second_node.id;
             edge.rating += added_danger;
             edge.rating = std::min(edge.rating, 10.0);
+            updateOppositeEdge(adj, edge.first_node.id, edge.second_node.id, edge.rating);
 
             if (hops[v] == -1) {
                 hops[v] = current_hop + 1;
                 q.push(v);
             }
+        }
+    }
+}
+
+void printGraph(const vector<vector<Edge>>& adj) {
+    for (int i=0;i<adj.size();i++) {
+        for (int j=0;j<adj[i].size();j++) {
+            std::cout<<"("<<adj[i][j].first_node.id<<"; "<<adj[i][j].second_node.id<<"; "<<adj[i][j].length<<"; "
+                    <<adj[i][j].rating<<")  ";
+        }
+        std::cout<<"\n";
+    }
+}
+
+void updateOppositeEdge(vector<vector<Edge>>& adj, const size_t& first, const size_t& second, double& newRating) {
+    for (int i=0;i<adj[second].size();i++) {
+        if (adj[second][i].second_node.id==first) {
+            newRating = std::min(newRating, 10.0);
+            adj[second][i].rating = newRating;
+            return;
         }
     }
 }
--- AoE/AoEScale.h
+++ AoE/AoEScale.h
@@ -6,11 +6,11 @@
 #include "../algorithms/graph_structure.hpp"
 using namespace std;
 
-// void updateNearEdges(vector<vector<pair<size_t,size_t>>>& adj, const pair<size_t,size_t>& srcEdge, const size_t& diffInRating);
-// void updateOppositeSide(vector<vector<pair<size_t,size_t>>>& adj, const size_t& v1, const size_t& other, double modifier);
-// void printGraph(const vector<vector<pair<size_t,size_t>>>& adj);
 
+void updateOppositeEdge(vector<vector<Edge>>& adj, const size_t& first, const size_t& second, double& newRating);
+void printGraph(const vector<vector<Edge>>& adj);
 void updateNearEdgesV2(vector<vector<Edge>>& adj, const size_t& srcNodeId, const double& modifier);
 std::queue<vector<Edge>> bfs(vector<vector<Edge>>& adj, const Node& src, vector<double>& dist);
+
 
 #endif //GIT_PUSH_AND_PRAY_AOESCALE_H
