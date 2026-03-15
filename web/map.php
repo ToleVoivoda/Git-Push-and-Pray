b7827bace343d6c54e5d4ecdaf09c431d0ba10c6
katerinacookies <sokolyt@gmail.com>
2026-03-15 02:02:41 +0200

    MAP SLAY

--- web/map.php
+++ web/map.php
@@ -1,38 +1,22 @@
-<?php
-    session_start();
-    //include "db_connection.php";
-?>
-
 <!DOCTYPE html>
-<html lang="bg">
-
+<html>
 <head>
-    <meta charset="UTF-8">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/index.css">
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-    <!--leaflet-->
-     <link rel="stylesheet" href="https://unpkg.com/leaflet/dist/leaflet.css"
-     integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY="
-     crossorigin=""/>
-      <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"
-     integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo="
-     crossorigin=""></script>
-    <title>Safe streets</title>
+    <title>Safe Route Map</title>
+
+    <link rel="stylesheet" href="https://unpkg.com/leaflet/dist/leaflet.css"/>
+
+    <style>
+        #map {
+            height: 600px;
+        }
+    </style>
 </head>
-
 <body>
-    <div>
-        <heading class="heading-main">Safe streets</heading>
-    </div>
 
-    <div>
-         <div id="map"></div>
-    </div>
+<div id="map"></div>
 
-    <script src="js/map.js"></script>
+<script src="https://unpkg.com/leaflet/dist/leaflet.js"></script>
+<script src="js/map.js"></script>
+
 </body>
 </html>
